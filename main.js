4e60d41a01b3fe81bb11b39f99bf8aa4f3302548
Ebenezer Jay Ingalsbe <[ebenezerjaymusic@gmail.com]>
2019-05-13 18:22:56 -0600

    Fortune saving to local stoarage

--- main.js
+++ main.js
@@ -4,14 +4,25 @@ var appendSection = document.querySelector('#article-append');
 
 enterButton.addEventListener('click', onEnter);
 
-
+var fortuneArray = JSON.parse(localStorage.getItem('fortune-array')) || [];
 function onEnter() {
-    appendFortune();
+	createFortuneObject();
 }
 
-function appendFortune() {
-    appendSection.innerHTML = `
-		<h3>${fortuneInput.value}</h3>
-		` + appendSection.innerHTML;
-		fortuneInput.value = '';
+function createFortuneObject() {
+	var fortuneId = Math.floor(Date.now());
+	var fortuneString = fortuneInput.value;
+
+	appendFortune(fortuneId,fortuneString);
+	var newFortune = new Fortune(fortuneId,fortuneString);
+	fortuneArray.push(newFortune);
+	newFortune.saveToStorage(fortuneArray);
+	console.log(fortuneArray);
+}
+
+function appendFortune(id,fortune) {
+	appendSection.innerHTML = `
+	<h3 id="appended-fortune" data-id="${id}">${fortune}</h3>
+	` + appendSection.innerHTML;
+	fortuneInput.value = '';
 }
