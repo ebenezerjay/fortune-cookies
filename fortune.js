4668b995edced722d95d262dbafa39cd8412423c
Ebenezer Jay Ingalsbe <[ebenezerjaymusic@gmail.com]>
2019-05-14 15:58:32 -0600

    Add fortune image and css to appended fortunes

--- fortune.js
+++ fortune.js
@@ -8,4 +8,18 @@ class Fortune {
 			localStorage.setItem('fortune-array', JSON.stringify(array));
 		}
 
+		deleteFromStorage(id) {
+			// var fortuneArray = this.pullFromStorage();
+			fortuneArray.splice(this.getIndex(id), 1);
+			this.saveToStorage(fortuneArray);
+		}
+
+		getIndex(id) {
+			return this.pullFromStorage().findIndex(fortune => fortune.id === id);
+		}
+		
+		pullFromStorage() {
+			return JSON.parse(localStorage.getItem('fortune-array'));
+		}
+
 }
--- main.js
+++ main.js
@@ -1,14 +1,22 @@
+// global variables
 var fortuneInput = document.querySelector('#fortune-input');
 var enterButton = document.querySelector('#enter-button');
 var appendSection = document.querySelector('#article-append');
-
-enterButton.addEventListener('click', onEnter);
+var fortuneLabel = document.querySelector('#img-label');
 
 var fortuneArray = JSON.parse(localStorage.getItem('fortune-array')) || [];
+
+// event listeners
+enterButton.addEventListener('click', onEnter);
+appendSection.addEventListener('click', deleteFortune);
+window.addEventListener('load',loadPreviousFortunes(fortuneArray));
+
+// fires these functions when enter button is clicked
 function onEnter() {
 	createFortuneObject();
 }
 
+// creates the Fortune object that is saved into local storage
 function createFortuneObject() {
 	var fortuneId = Math.floor(Date.now());
 	var fortuneString = fortuneInput.value;
@@ -20,9 +28,38 @@ function createFortuneObject() {
 	console.log(fortuneArray);
 }
 
+// appends the inputed fortune to the dom
 function appendFortune(id,fortune) {
+	appendOntoImg();
 	appendSection.innerHTML = `
-	<h3 id="appended-fortune" data-id="${id}">${fortune}</h3>
+		<div class="article-appended-fortune flex" id="article-appended-fortune">
+			<h3 id="appended-fortune" data-id="${id}">${fortune}
+			<button type="button" class="delete-button" id="delete-button">X</button>
+			</h3>
+		</div>
 	` + appendSection.innerHTML;
 	fortuneInput.value = '';
 }
+
+function appendOntoImg() {
+	fortuneLabel.innerText = fortuneInput.value;
+}
+
+// persists object data on page load
+function loadPreviousFortunes() {
+    for (var i = 0; i < fortuneArray.length; i++) {
+      appendFortune(fortuneArray[i].id, fortuneArray[i].fortune);
+    }
+}
+
+// deletes fortune from dom and local storage
+function deleteFortune(e) {
+	var fortuneInstance = new Fortune();
+	var articleId = e.target.parentElement.dataset.id;
+	fortuneInstance.deleteFromStorage(parseInt(articleId));
+	if (e.target.classList.contains('delete-button')) {
+		e.target.parentElement.remove();
+	}
+	e.preventDefault();
+}
+
