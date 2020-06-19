2cde33b25cc649d8de1f51c52301804cb5d81253
Ebenezer Jay Ingalsbe <[ebenezerjaymusic@gmail.com]>
2020-06-19 10:23:30 -0600

    Fix bugs, and add ajax call for submit

--- contact.php
+++ contact.php
@@ -1,38 +1,33 @@
 <?php
+// decode json
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST)){
+	$_POST = json_decode(file_get_contents('php://input'), true);
+}
 
-// check if contact form was submitted
-
-if(isset($_POST['contact-enter'])) {
+	// connect to database
 	$fortuneDatabaseContact = new mysqli("localhost:3306", "fortuned_eJI", "Helpontheway2112!", "fortuned_Fortunes");
-	$contactResponse = "";
 
 	// Check connection
 	if ($fortuneDatabaseContact->connect_error) {
 		die('Connect error: ' . $fortuneDatabaseContact->connect_errno . ': ' . $fortuneDatabaseContact->connect_error);
 	}
 
-	// get input data
-	$contactName = $_POST['contact-name'];
-	// $contactEmail = $_POST['contact-email'];
-	// $contactTopic = $_POST['contact-topic'];
-	// $contactMessage = $_POST['contact-message'];
-
-	$contactResponse = "Thank you " . $contactName . " . " . " Your message has been submitted.";
+	$message = "Your message was sent.";
 
 	// insert into database
-	$contactData = "INSERT INTO contactFormData (contactName,contactEmail,contactTopic,contactMessage) VALUES ('{$fortuneDatabaseContact->real_escape_string($_POST['contactName'])}', 
+
+	$contactData = "INSERT INTO contactFormData (contactName,contactEmail,contactTopic,contactMessage) VALUES 
+  ('{$fortuneDatabaseContact->real_escape_string($_POST['contactName'])}', 
 	'{$fortuneDatabaseContact->real_escape_string($_POST['contactEmail'])}',
 	'{$fortuneDatabaseContact->real_escape_string($_POST['contactTopic'])}',
 	'{$fortuneDatabaseContact->real_escape_string($_POST['contactMessage'])}')";
 
-	$insertContact = $fortuneDatabaseContact->query($contactData);
+	$insertData = $fortuneDatabaseContact->query($contactData);
 
-	// print response for user after submission
-	if ($insertContact) {
-		echo $contactResponse;
+	if ($insertFortune) {
+		echo "Thank you, ". $contactName. " " . $message;
 	} else {
-		die("Error: {$fortuneDatabaseContact->errno} : {$fortuneDatabaseContact->error}");
+		die("Error: {$fortuneDatabase->errno} : {$fortuneDatabase->error}");
 	}
-}
 
 ?>
